03967db2fd13851b4892de5c572b671ac6066825
Michael Tiller <michael.tiller@xogeny.com>
2019-02-09 14:59:36 -0500

    Working through second column

--- src/topics/column2/logic_ops.tsx
+++ src/topics/column2/logic_ops.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { Topic } from "../../components/topic";
 
+const warning = (
+    <div style={{ padding: 5 }}>
+        <p className="seminarrow">
+            These role of these operators is self-explanatory. However, it is worth pointing out that the relational
+            operators <code>&lt;</code>, <code>&gt;</code>, <code>&lt;=</code>
+            and <code>&gt;=</code> will generate events when used in conjunction with continuous time variables. More
+            discussion of such events and how to avoid them can be found in{" "}
+            <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=29" target="_blank">
+                &sect;3.7.3
+            </a>
+            .
+        </p>
+    </div>
+);
 export const logicalOperators = (
     <Topic title="Logical Operators">
         <Topic.Item
@@ -11,6 +25,7 @@ export const logicalOperators = (
                     </code>
                 </b>
             }
+            warning={warning}
         />
     </Topic>
 );
--- src/topics/column2/std_enums.tsx
+++ src/topics/column2/std_enums.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { Topic } from "../../components/topic";
 
+const assertionLevel = (
+  <div style={{padding: 5}}>
+  <code><span className="comment">// From <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=86" target="_blank">&sect;8.3.7</a></span></code><br/>
+  <code><b>type</b> <span className="typename">AssertionLevel</span> = <b>enumeration</b>(warning, error);</code>
+  </div>
+);
+
+const stateSelect = (
+  <div style={{padding: 5}}>
+  <code><span className="comment">// From <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=52" target="_blank">&sect;4.8.8.1</a></span></code><br/>
+  <code><b>type</b> <span className="typename">StateSelect</span> = <b>enumeration</b>(never, avoid, default, prefer, always);</code>
+  </div>
+)
 export const standardEnumerations = (
     <Topic title="Standard Enumerations">
         <Topic.Item
@@ -9,6 +22,7 @@ export const standardEnumerations = (
                     <code>AssertionLevel</code>
                 </b>
             }
+            comment={assertionLevel}
         />
         <Topic.Item
             title={
@@ -16,6 +30,7 @@ export const standardEnumerations = (
                     <code>StateSelect</code>
                 </b>
             }
+            comment={stateSelect}
         />
     </Topic>
 );
