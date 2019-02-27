5f8d8eaccc7a88b47280fed3a4e8941959f5767b
Michael Tiller <michael.tiller@xogeny.com>
2019-02-26 21:38:06 -0500

    Column 3 finished

--- src/topics/column3/event_ops.tsx
+++ src/topics/column3/event_ops.tsx
@@ -1,30 +1,304 @@
 import React from "react";
-import { Topic } from "../../components/topic";
+import { Topic, TopicProps } from "../../components/topic";
 
-const operators = [
-    "pre",
-    "noEvent",
-    "change",
-    "edge",
-    "initial",
-    "terminal",
-    "reinit",
-    "sample",
-    "smooth",
-    "terminate",
-];
-
-export const eventOperators = (
-    <Topic title="Event Operators">
-        {operators.map((op, i) => (
-            <Topic.Item
-                key={i}
-                title={
-                    <b>
-                        <code>{op}</code>
-                    </b>
-                }
-            />
-        ))}
-    </Topic>
+const pre = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code>pre(y)</code>
+            </td>
+            <td className="seminarrow">
+                At an event, the <code>pre</code> operator is used to refer to the value of a variable prior to that
+                event. There are several caveats related to the use of <code>pre</code>. These can be found in{" "}
+                <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=29" target="_blank">
+                    &sect;3.7.3
+                </a>
+                .
+            </td>
+        </tr>
+    </table>
 );
+
+const noEvent = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code>noEvent(expr)</code>
+            </td>
+            <td className="seminarrow">
+                The <code>noEvent</code> operator suppresses the generation of events by any event generating operators
+                or functions in the expression <code>expr</code>. Further information about the <code>noEvent</code>{" "}
+                operator can be found in{" "}
+                <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=29" target="_blank">
+                    &sect;3.7.3.2
+                </a>{" "}
+                and{" "}
+                <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=88" target="_blank">
+                    &sect;8.5
+                </a>
+                .
+            </td>
+        </tr>
+    </table>
+);
+
+const change = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code>change(v)</code>
+            </td>
+            <td className="seminarrow">
+                The expression <code>change(v)</code>
+                becomes{" "}
+                <code>
+                    <b>true</b>
+                </code>{" "}
+                only for the instant that the value of the variable <code>v</code> changes (<i>i.e.,</i>{" "}
+                <code>{`v<>pre(v)`}</code>). See the <code>pre</code>
+                operator and/or{" "}
+                <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=30" target="_blank">
+                    &sect;3.7.3.1
+                </a>{" "}
+                for more details and restrictions.
+            </td>
+        </tr>
+    </table>
+);
+
+const edge = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code>edge(b)</code>
+            </td>
+            <td className="seminarrow">
+                The <code>edge</code> operator becomes{" "}
+                <code>
+                    <b>true</b>
+                </code>{" "}
+                only at the instant when the expression <code>b</code> goes from{" "}
+                <code>
+                    <b>false</b>
+                </code>
+                to{" "}
+                <code>
+                    <b>true</b>
+                </code>{" "}
+                (<i>i.e.,</i> <code>b and not pre(b)</code>). See the <code>pre</code> operator and/or{" "}
+                <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=30" target="_blank">
+                    &sect;3.7.3.1
+                </a>{" "}
+                for more details and restrictions.
+            </td>
+        </tr>
+    </table>
+);
+const initial = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code>initial()</code>
+            </td>
+            <td className="seminarrow">
+                The <code>initial</code> operator is{" "}
+                <code>
+                    <b>true</b>
+                </code>
+                during the initialization phase of the simulation.
+            </td>
+        </tr>
+    </table>
+);
+
+const terminal = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code>terminal()</code>
+            </td>
+            <td className="seminarrow">
+                The <code>terminal</code> operator becomes{" "}
+                <code>
+                    <b>true</b>
+                </code>{" "}
+                at the end of a <b>successful</b> simulation.
+            </td>
+        </tr>
+    </table>
+);
+
+const reinit = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code>reinit(x, expr)</code>
+            </td>
+            <td className="seminarrow">
+                The <code>reinit</code> operator sets the value of the variable <code>x</code> to be equal to the of{" "}
+                <code>expr</code>. Note, <code>reinit</code> can only be used within a
+                <code>
+                    <b>when</b>
+                </code>{" "}
+                clause. For more details on the usage and restrictions of <code>reinit</code>, see{" "}
+                <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=86" target="_blank">
+                    &sect;8.3.6
+                </a>
+                .
+            </td>
+        </tr>
+    </table>
+);
+
+const sample = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code>sample(start, interval)</code>
+            </td>
+            <td className="seminarrow">
+                The <code>sample</code> operator because true when <code>time>=start</code> and again every{" "}
+                <code>interval</code> seconds after that. Further details can be found in{" "}
+                <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=29" target="_blank">
+                    &sect;3.7.3
+                </a>
+                .
+            </td>
+        </tr>
+    </table>
+);
+
+const smooth = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code>smooth(p, expr)</code>
+            </td>
+            <td className="seminarrow">
+                When evaluated, the <code>smooth</code> operator simply returns the value of the expression{" "}
+                <code>expr</code>. But it also acts as a guarantee that <code>expr</code> is
+                <code>p</code> times continuously differentiable. For further details, see{" "}
+                <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=30" target="_blank">
+                    &sect;3.7.3.2
+                </a>
+                .
+            </td>
+        </tr>
+    </table>
+);
+
+const terminate = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code>terminate()</code>
+            </td>
+            <td className="seminarrow">
+                The <code>terminate</code> operator instructs the underlying simulation environment to terminate the
+                simulation after the completion of the next integration step or event iteration. See{" "}
+                <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=87" target="_blank">
+                    &sect;8.3.8
+                </a>{" "}
+                for more details.
+            </td>
+        </tr>
+    </table>
+);
+
+const data: TopicProps = {
+    title: "Event Operators",
+    comment: (
+        <span>
+            {" "}
+            These event related operators are discussed in{" "}
+            <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=29" target="_blank">
+                &sect;3.7.3
+            </a>
+            .
+        </span>
+    ),
+    items: [
+        {
+            title: <code>pre</code>,
+            example: pre,
+        },
+        {
+            title: <code>noEvent</code>,
+            example: noEvent,
+        },
+        {
+            title: <code>change</code>,
+            example: change,
+        },
+        {
+            title: <code>edge</code>,
+            example: edge,
+        },
+        {
+            title: <code>initial</code>,
+            example: initial,
+        },
+        {
+            title: <code>terminal</code>,
+            example: terminal,
+        },
+        {
+            title: <code>reinit</code>,
+            example: pre,
+        },
+        {
+            title: <code>sample</code>,
+            example: sample,
+        },
+        {
+            title: <code>smooth</code>,
+            example: smooth,
+        },
+        {
+            title: <code>terminate</code>,
+            example: terminate,
+        },
+    ],
+};
+
+export const eventOperators = <Topic {...data} />;
--- src/topics/column3/red_ops.tsx
+++ src/topics/column3/red_ops.tsx
@@ -1,19 +1,192 @@
 import React from "react";
-import { Topic } from "../../components/topic";
+import { Topic, TopicProps } from "../../components/topic";
 
-const operators = ["max", "min", "product", "sum"];
+const max = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
 
-export const reductionOperators = (
-    <Topic title="Reduction Operators">
-        {operators.map((op, i) => (
-            <Topic.Item
-                key={i}
-                title={
-                    <b>
-                        <code>{op}</code>
-                    </b>
-                }
-            />
-        ))}
-    </Topic>
+        <tr>
+            <td>
+                <code className="nobreak">max(A)</code>
+            </td>
+            <td className="seminarrow">
+                When applied to an array, the <code>max</code> function returns the element of the array with the
+                greatest value. For example,
+                <br />
+                <div className="indented">
+                    <code>max({`[1, 5; 7, 3]`})</code> returns <code>7</code>
+                    <br />
+                    <code>max({`{3, 8, 1}`})</code> returns <code>8</code>
+                </div>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <code className="nobreak">max(x, y)</code>
+            </td>
+            <td className="seminarrow">
+                When used with two scalar arguments, <code>max</code>
+                returns the greater of the two arguments.
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <code className="nobreak">max(e, for i in u, j in v, ...)&nbsp;&nbsp;</code>
+            </td>
+            <td className="seminarrow">
+                Returns the greatest value among all possible values of <code>e</code> over the range of all possible
+                index combinations.
+            </td>
+        </tr>
+    </table>
 );
+
+const min = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+
+        <tr>
+            <td>
+                <code className="nobreak">min(A)</code>
+            </td>
+            <td className="seminarrow">
+                When applied to an array, the <code>min</code> function returns the element of the array with the least
+                value. For example,
+                <br />
+                <div className="indented">
+                    <code>min({`[1, 5; 7, 3]`})</code> returns <code>1</code>
+                    <br />
+                    <code>min({`{3, 8, 1}`})</code> returns <code>1</code>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <code className="nobreak">min(x, y)</code>
+            </td>
+            <td className="seminarrow">
+                When used with two scalar arguments, <code>min</code>
+                returns the lesser of the two arguments.
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <code className="nobreak">min(e, for i in u, j in v, ...)&nbsp;&nbsp;</code>
+            </td>
+            <td className="seminarrow">
+                Returns the least value among all possible values of <code>e</code> over the range of all possible index
+                combinations.
+            </td>
+        </tr>
+    </table>
+);
+
+const product = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code className="nobreak">product(A)</code>
+            </td>
+            <td className="seminarrow">
+                When applied to an array, the <code>product</code>
+                function returns the product of all elements in the array. For example,
+                <br />
+                <div className="indented">
+                    <code>product({`[1, 5; 7, 3]`})</code> returns <code>105</code>
+                    <br />
+                    <code>product({`{3, 8, 1}`})</code> returns <code>24</code>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <code className="nobreak">product(e, for i in u, j in v, ...)&nbsp;&nbsp;</code>
+            </td>
+            <td className="seminarrow">
+                Returns the product of all values of <code>e</code> over all possible index combinations.
+            </td>
+        </tr>
+    </table>
+);
+const sum = (
+    <table className="table">
+        <tr>
+            <th>Usage</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            <td>
+                <code className="nobreak">sum(A)</code>
+            </td>
+            <td className="seminarrow">
+                When applied to an array, the <code>sum</code>
+                function returns the sum of all elements in the array. For example,
+                <br />
+                <div className="indented">
+                    <code>sum({`[1, 5; 7, 3]`})</code> returns <code>16</code>
+                    <br />
+                    <code>sum({`{3, 8, 1}`})</code> returns <code>12</code>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <code className="nobreak">sum(e, for i in u, j in v, ...)</code>
+            </td>
+            <td className="seminarrow">
+                Returns the sum of all values of <code>e</code> over all possible index combinations.
+            </td>
+        </tr>
+    </table>
+);
+
+const data: TopicProps = {
+    title: "Reduction Operators",
+    comment: (
+        <span>
+            Reduction operators are discussed in{" "}
+            <a href="https://www.modelica.org/documents/ModelicaSpec33.pdf#page=113" target="_blank">
+                &sect;10.3.4
+            </a>
+            .
+        </span>
+    ),
+    items: [
+        {
+            title: <code>max</code>,
+            example: max,
+        },
+        { title: <code>min</code>, example: min },
+        { title: <code>product</code>, example: product },
+        { title: <code>sum</code>, example: sum },
+    ],
+};
+
+export const reductionOperators = <Topic {...data} />;
+
+// export const reductionOperators = (
+//     <Topic title="Reduction Operators">
+//         {operators.map((op, i) => (
+//             <Topic.Item
+//                 key={i}
+//                 title={
+//                     <b>
+//                         <code>{op}</code>
+//                     </b>
+//                 }
+//             />
+//         ))}
+//     </Topic>
+// );
