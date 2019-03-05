440197059ff4585d42adb18ee76cda1b41a6cede
Michael Tiller <michael.tiller@xogeny.com>
2019-03-05 11:49:08 +0100

    Tweaked CSS for mobile
    This isn't really what I want (yet).  I need to research
    how I want to deal with this.  I suspect media queries
    will be required.

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,6 +29,7 @@ import {
 interface Column {
     grow: number;
     min: number;
+    max: number;
     elements: JSX.Element[];
 }
 
@@ -36,11 +37,13 @@ const ordering: Column[] = [
     {
         grow: 1,
         min: 10,
+        max: 25,
         elements: [lexicalRules, specializedClasses, classDefinitions, connectorQualifiers, variabilityQualifiers],
     },
     {
         grow: 1,
-        min: 15,
+        min: 30,
+        max: 35,
         elements: [
             builtinTypes,
             standardEnumerations,
@@ -52,8 +55,8 @@ const ordering: Column[] = [
             conversionFunctions,
         ],
     },
-    { grow: 1, min: 10, elements: [specialOperators, reductionOperators, eventOperators] },
-    { grow: 1, min: 10, elements: [statements, complexExpressions, operatorOperator, standardAnnotations] },
+    { grow: 1, min: 10, max: 25, elements: [specialOperators, reductionOperators, eventOperators] },
+    { grow: 1, min: 10, max: 25, elements: [statements, complexExpressions, operatorOperator, standardAnnotations] },
 ];
 
 export interface MainState {
@@ -84,13 +87,17 @@ export class Main extends React.Component<{}, MainState> {
     render() {
         return (
             <searchContext.Provider value={{ search: this.state.search, setTerm: this.setSearch }}>
-                <div style={{ background: "#f8f8fc" }}>
+                <div style={{ background: "#f8f8fc", fontSize: "12px" }}>
                     <h1 style={headerStyle}>Modelica Web Reference</h1>
                     <div style={{ display: "flex" }}>
                         {ordering.map((column, ci) => {
                             return (
                                 <div
-                                    style={{ flexGrow: column.grow, minWidth: `${column.min} em` }}
+                                    style={{
+                                        flexGrow: column.grow,
+                                        minWidth: `${column.min}em`,
+                                        maxWidth: `${column.max}vw`,
+                                    }}
                                     key={ci}
                                     className="topicColumn"
                                 >
@@ -102,6 +109,9 @@ export class Main extends React.Component<{}, MainState> {
                                 </div>
                             );
                         })}
+                    </div>
+                    <div style={{ float: "right", margin: 5 }}>
+                        <b>Copyright 2019 - Michael Tiller</b>
                     </div>
                     <div style={{ display: "flex", margin: 5 }}>
                         <span style={legendStyle}>
